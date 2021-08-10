7861ac6ac3384598ff57239e100772f22ce2e0ab
Kordero <elpadredelcordero@gmail.com>
2021-08-10 15:30:12 -0500

    mejora del rss

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,9 +2,17 @@ const apiGhost = new(require("../api/ghost"));
 const RSS=require("rss");
 const fs=require("fs");
 const path=require("path");
-const apiCloudStorage=new(require("../api/gcp/CloudStorage"))
+const apiCloudStorage=new(require("../api/gcp/CloudStorage"));
+//documentation https://mailchimp.com/es/help/rss-merge-tags/
 class Feed {
-
+    _endpointRebase(url){
+        const pattern=/^https?:\/\/www.onsv.gob.pe/;
+        return url.replace(pattern,`${process.env.URL_PATH}/post`);
+    }
+    _transformHttps=(element)=>{
+        const patttern = /^http:/ 
+        return element.replace(patttern,'https:');
+    }
     _getSettings=async()=>{
         const result= await apiGhost.getSettings();
         //return result
@@ -13,15 +21,17 @@ class Feed {
     _getArticles=async()=>{
         //Se obtienen todos los posts
         const posts = await apiGhost.getPosts(8,"tags,authors","featured:false", "published_at DESC");
+        //console.log(posts)
         let articles =[];
         posts.forEach(post => {
             let article={
+                image:this._transformHttps(post.feature_image),
                 title:post.title,
                 description:post.excerpt,
-                url:post.url,
+                url:this._endpointRebase(post.url),
                 publishedDate:post.created_at
             }
-
+            console.log(article.image)
             articles.push(article);
             
         });
@@ -39,13 +49,20 @@ class Feed {
         const feed =new RSS({
             title:blog.title,
             description:blog.description,
-            author:blog.author
+            author:blog.author,
+            custom_namespaces:{
+                'media':`${process.env.URL_PATH}`
+            }
         });
         for(const article of blog.articles){
             feed.item({
                 title:article.title,
                 description:article.description,
-                date:article.publishedDate
+                date:article.publishedDate,
+                url:article.url,
+                custom_elements:[
+                    {'media:content':{_attr:{url:article.image}}}
+                ]
             })
         }
         return feed.xml({indent:true});
